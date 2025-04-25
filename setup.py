6cb54af242cc09581dca73161681a2a90852c9d9
Aleksandr Dezhin <me@dezhin.net>
2025-04-25 18:06:24 +0000

    Bump minial Python version to 3.10
    NGW-1559

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with io.open("VERSION", "r") as fd:
     VERSION = fd.read().rstrip()
 
 requires = [
-    "nextgisweb>=5.1.0.dev3",
+    "nextgisweb>=5.1.0.dev11",
 ]
 
 entry_points = {
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.8,<4",
+    python_requires=">=3.10",
     install_requires=requires,
     entry_points=entry_points,
 )
